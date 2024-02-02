df26e228e4e6aca748ef259b65da70331b49eea2
ZLY201 <951711127@qq.com>
2024-02-02 15:25:12 +0800

    feat: Array.at

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export default function render(container: HTMLElement) {
+  const a = [12];
+  console.log(a);
+  console.log(a.at(0));
   container.innerHTML =
 `<div style="width: 100px; height: 100px; background-color: red">
   <button>click</button>
